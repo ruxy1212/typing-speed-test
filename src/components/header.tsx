fe57aab6a1e3051a4c35ec010c396abc515bb1a8
Russell Oje <85977511+ruxy1212@users.noreply.github.com>
2026-01-03 12:49:34 +0000

    Built header component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,14 +1,21 @@
 import LogoLarge from "@/assets/logo-large.svg"
 import LogoSmall from "@/assets/logo-small.svg"
+import TrophyIcon from "@/assets/icon-personal-best.svg"
 
 export default function Header() {
     return (
-        <header className="w-full max-w-[1200px] flex justify-between items-center">
-            <div className="flex gap-2 items-center">
-                <picture>
-                    <source media="(min-width: 768px)" srcSet={LogoLarge} />
-                    <img src={LogoSmall} alt="Typing Speed Test logo" />
-                </picture>
+        <header className="w-full max-w-[1200px] mx-auto flex justify-between items-center">
+            <div>
+                <LogoLarge className="hidden md:block h-10" />
+                <LogoSmall className="block md:hidden h-8" />
+            </div>
+            <div className="flex gap-2.5 items-center justify-center">
+                <TrophyIcon className="h-4 md:h-[18px]" />
+                <h3 className="text-ts-neutral-400 text-lg">
+                    <span className="hidden md:inline">Personal best:</span>
+                    <span className="md:hidden">Best:</span>{' '}
+                    <span className="text-ts-neutral-0">92 WPM</span>
+                </h3>
             </div>
         </header>
     )
