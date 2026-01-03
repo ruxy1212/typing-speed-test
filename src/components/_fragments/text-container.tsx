f97dd332fd51c206ac16d448586a12f574b42fd0
Russell Oje <85977511+ruxy1212@users.noreply.github.com>
2026-01-03 23:56:28 +0000

    Fixed start button positioning

--- src/components/_fragments/text-container.tsx
+++ src/components/_fragments/text-container.tsx
@@ -4,8 +4,8 @@ export default function TextContainer({ children, isTyping, setIsTyping }: { chi
             {children}
         </div>
         {!isTyping && (
-            <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center">
-                <div className="flex flex-col justify-center items-center gap-4 md:gap-5">
+            <div className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 top-[clamp(20vh,50%,30vh)]">
+                <div className="flex flex-col justify-center items-center gap-4 whitespace-nowrap md:gap-5">
                     <button onClick={() => setIsTyping(true)} className="text-center bg-ts-blue-600 text-ts-neutral-0 text-xl rounded-lg cursor-pointer px-4 py-2 md:px-6 md:py-4 transition-colors hover:bg-ts-blue-400">Start Typing Test</button>
                     <span className="text-center text-ts-neutral-0 text-xl">Or click the text and start typing</span>
                 </div>
