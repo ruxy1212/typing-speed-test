301913e7dbc036b5ba842d19d3471748332a182b
Russell Oje <85977511+ruxy1212@users.noreply.github.com>
2026-01-03 15:23:19 +0000

    Styled text container

--- src/components/_fragments/text-container.tsx
+++ src/components/_fragments/text-container.tsx
@@ -1,3 +1,3 @@
 export default function TextContainer({ children }: { children: React.ReactNode }) {
-    return <div className="text-ts-neutral-300">{children}</div>;
+    return <div className="text-ts-neutral-400 leading-normal text-3xl md:text-4xl">{children}</div>;
 }
